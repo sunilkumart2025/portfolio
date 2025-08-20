55bfdc65f87f7ad49aaf813511de789a1dc2ac09
sunilkumar_25 <suniltamil2025@gmail.com>
2025-08-20 17:01:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 function sayhello() {
     window.location.href = "mailto:skysunil2025@gmail.com";
 }
+
+function project_1_source() {
+    window.location.href = "https://srm-gamma.vercel.app/";
+}
+
+function project_1_contribute() {
+    window.location.href = "https://docs.google.com/forms/d/e/1FAIpQLScLb8eugI6UKjO-g2Fchrjiwf2Jm0cneVyKT-6Ee3edxsXXgQ/viewform?usp=header";
+}
